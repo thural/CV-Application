d813c79b9118451e7baf8d86630b4f4697138bef
Tural Musaibov <tural.musaibov@gmail.com>
2022-09-11 21:08:16 +0400

    refactored Skills component to useReducer

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,64 +1,114 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import uniqid from "uniqid";
 
+function listReducer(state, action) {
+
+  switch (action.type) {
+    case 'add':
+      const update = [...state];
+      update.push(action.skill);
+      return update;
+    case 'edit':
+      return state.map(skill => {
+        if (skill.id == action.id) {
+          skill.readOnly = false
+          return skill
+        } else return skill
+      });
+    case 'save':
+      return state.map(skill => {
+        if (skill.id == action.id) {
+          skill.value = action.value
+          return skill
+        } else return skill
+      });
+    default:
+      return state
+  }
+};
+
+function skillReducer(state, skillName) {
+  const update = { ...state };
+  update.value = skillName;
+  return update
+};
+
+
+///////////////////////////////////////////////////////////////
+
 const Skills = () => {
-  const [skills, setSkills] = useState({
 
-    all: [
+  const [list, setList] = useReducer(listReducer,
+    [
       {
-        skill: "",
+        value: "Javascript",
         id: uniqid(),
-        visible: false
-      }
-    ],
+        readOnly: true,
+      },
+    ]
+  );
 
-    current: {
-      skill: "",
+  const [skill, setSkill] = useReducer(skillReducer,
+    {
+      value: "",
       id: uniqid(),
-      visible: false
+      readOnly: true,
     }
-    
-  });
+  );
+
+  const add = () => {
+    setList({ skill, type: "add" })
+  };
+
+  const edit = (id) => {
+    setList({ id, type: "edit" })
+  };
+
+  const save = (id, value) => {
+    setList({ id, type: "save", value })
+  };
 
   const handleChange = (e) => {
-    setSkills({
-      current: {
-        skill: e.target.value
-      }
-    })
-  };
-
-  const showForm = () => {
-    setSkills({ current: { visible: true } })
-  };
-
-  const addSkill = () => {
-    setSkills({ all: all.concat(current) })
+    setSkill(e.target.value)
   };
 
 
   return (
     <div>
+      <h3>Skills</h3>
+
       {
-        skills.all.map(skill => (
+        list.map(skill => (
 
           <form key={skill.id}>
-            <h3>Skills</h3>
             <input
               type="input"
               id="skill"
               name="skill"
               placeholder="skill"
-              onChange={(e) => handleChange(e)}
+              readOnly={skill.readOnly}
             >
             </input>
-            <button type="submit" onClick={addSkill()}>save</button>
+            {skill.readOnly && <button type="submit" onClick={edit(skill.id)}>edit</button>}
+            {!skill.readOnly && <button type="submit" onClick={(e) => save(skill.id, e.target.value)}>save</button>}
           </form>
 
-        ))
-      }
-      {!skills.current.visible && <button onClick={showForm()}>add</button>}
+        ))}
+
+      <form key={skill.id}>
+        <input
+          type="input"
+          id="skill"
+          name="skill"
+          placeholder="skill"
+          onChange={(e) => handleChange(e)}
+        >
+        </input>
+        <button type="submit" onClick={add()}>add</button>
+      </form>
+
     </div>
   )
-}
+};
+
 export default Skills
